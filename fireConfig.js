b2b7458e97f43e0f7b7d25dcc630f430aac9a5ef
sannika27 <bacomosteffiannika@gmail.com>
2025-05-17 17:12:25 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD7E3965kXYMJQiRKuwP6Aaz1mR_V_KsZI",
+  authDomain: "bacomo-crud.firebaseapp.com",
+  databaseURL: "https://bacomo-crud-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bacomo-crud",
+  storageBucket: "bacomo-crud.firebasestorage.app",
+  messagingSenderId: "1064290607586",
+  appId: "1:1064290607586:web:ba04b171e3afaad3a099bb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
